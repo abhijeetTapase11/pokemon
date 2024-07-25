4df03a1d3d31f16d3670bb07caada38e25f01187
abhijeetTapase11 <abhijit.tapase@arthmate.co.in>
2024-07-25 10:27:31 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import AllCards from './components/AllCards';
 import PokemonDetail from './components/PokemonDetail';
 import LoginPage from './components/LoginPage';
+import { useSelector } from 'react-redux';
 
 const App = () => {
+  const PrivateRoute = ({ children }) => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/auth/:username" element={<AllCards />} />
-        <Route path="/auth/:username/:pokemonName" element={<PokemonDetail />} />
+        <Route path="/auth/:username" element={<PrivateRoute><AllCards /></PrivateRoute>} />
+        <Route path="/auth/:username/:pokemonName" element={<PrivateRoute><PokemonDetail /></PrivateRoute>} />
       </Routes>
     </Router>
   );
